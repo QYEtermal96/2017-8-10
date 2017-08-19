5de3b1cdfa2ec05d19b94b1d7562e5891cbdb1c5
e <1456958517@qq.com>
2017-08-19 19:58:48 +0800

    first

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,21 +10,11 @@ var main = require("../lib/main.js");
 
 
 describe("测试描述", function(){
-    sinon.spy(console, 'log');
-
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
-    });
-
-    it("测试用例2", function(){
-
-        main();
+    it("[1,2,3,4,5,4,3,2,1] To '1'", function(){
+        sinon.spy(console, 'log');
+        main([1,2,3,4,5,4,3,2,1]);
         var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var expect_string = '5';
 
         expect(expect_string).to.equal(result);
     });
